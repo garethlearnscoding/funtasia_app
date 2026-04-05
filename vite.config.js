b5413423346a0ef5d4089ec79c21c783e286cd73
Gareth Chng <goofygoobers20230313@gmail.com>
2026-04-05 23:23:48 +0800

    casey basey wasey

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base:"/funtasia_app/",  
   plugins: [
     tailwindcss(),
     VitePWA({

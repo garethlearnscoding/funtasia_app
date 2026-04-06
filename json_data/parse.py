f9e18968c186858dbfdad70466969a4447459fbb
Gareth Chng <goofygoobers20230313@gmail.com>
2026-04-06 12:49:11 +0800

    Initial commit

--- json_data/parse.py
+++
@@ -1,9 +0,0 @@
-import json
-import csv
-
-with open("Booth Data.csv", "r") as file:
-    csv_reader = csv.DictReader(file)
-    booths = {row["booth_id"]: row for row in csv_reader}
-
-with open("booths.json", "w") as file:
-    json.dump(booths, file, indent=2)

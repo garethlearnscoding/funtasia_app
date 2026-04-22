ea18f112fa6cf20738b35cf4479648cda5a2924b
Gareth | Broken Toaster <goofygoobers20230313@gmail.com>
2026-04-22 14:31:25 +0800

    something

--- json_data/parse.py
+++ json_data/parse.py
@@ -5,25 +5,17 @@ with open("Booth Data - Sheet2.csv", "r") as file:
     data = list(csv.DictReader(file))
 
 print(data)
-"""
-for i in data:
-    tmp = i["Zone"]
-    print(tmp)
-    z,_ = tmp.split("-",1)
-    i["Zone"] = z
 
-length = len(data)
-json_data = {"l1":data[:length//3], "l2":data[length//3:2*length//3], "l3":data[2*length//3:]}
-"""
 
 json_data = {}
 for i in data:
-    i["Level"] = "l"+i["Level"]
+    booth_id = i.pop("Booth ID")
+    level = "l" + i.pop("Level")
     if "," in i["Tags"]:
         i["Tags"] = i["Tags"].split(",")
-    if i["Level"] not in json_data:
-        json_data[i["Level"]] = []
-    json_data[i["Level"]].append(i)
+    if level not in json_data:
+        json_data[level] = {}
+    json_data[level][booth_id] = i
 
 with open("funtasia_data.json", "w") as file:
     json.dump(json_data, file, indent=2)

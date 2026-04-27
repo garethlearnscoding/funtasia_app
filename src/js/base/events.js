b1a1eb70a07cbc6a7e2b66764af7025f95b1c43f
ffgtfgh <152868614+ffgtfgh53@users.noreply.github.com>
2026-04-27 14:19:27 +0800

    feat: Add sticky title to events timeline

--- src/js/base/events.js
+++ src/js/base/events.js
@@ -47,14 +47,15 @@ export async function switchEventCategory(category) {
         }
 
         let html = `
-        <header class="mb-8 w-full text-left">
-            <div class="flex flex-row mb-1 justify-items-center">
-                <h1 class="font-headline text-3xl font-bold tracking-tight text-ctp-text leading-none mr-2">Schedule</h1>
+        <header class="mb-8 w-full text-left sticky top-0 left-[-12] bg-ctp-base text-ctp-base z-30">
+            <div class="flex flex-row mb-1 justify-items-center w-full">
+                <h1 class="font-headline text-3xl font-bold tracking-tight text-ctp-text leading-none mr-2 sticky top-0">Schedule</h1>
                 <span class="events-location cursor-pointer hover:opacity-70 transition-opacity active:scale-95" data-booth-id="${data.location}">
                     <span class="material-symbols-outlined text-[12px]">location_on</span>${data.location}
                 </span>
             </div>
-            <p class="text-ctp-subtext0 font-body text-sm">${data.subtitle || ''}</p>            
+            <p class="text-ctp-subtext0 font-body text-sm w-full">${data.subtitle || ''}</p>
+            <div class="absolute left-[-6px] top-[-32px] w-[calc(100%+12px)] bg-ctp-base -z-10 h-28"></div> 
         </header>
         <div id="events-timeline">
         `;
@@ -137,14 +138,11 @@ export async function switchEventCategory(category) {
                     </div>
             `;
 
-            if (!ev.isSessionHeader) {
+            if (!ev.isSessionHeader && ev.description) {
                 html += `
                 <div class="${boxClass} events-item-body">
                     ${ev.description ? `<p class="${status === 'past' ? 'text-ctp-subtext0' : 'text-ctp-subtext1'} events-item-description">${ev.description}</p>` : ''}
-                    
-                    <div class="flex items-center gap-2 flex-wrap">
-                    ${tagsHtml}
-                    </div>
+
                 </div>
                 `;
             }
